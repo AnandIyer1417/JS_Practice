a2683cbef51f0de3e8e46d9ab8822c4270457dec
Anand1417 <ai6774@srmist.edu.in>
2021-09-06 04:06:53 +0530

    JS Console Basics

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log("Hello Everyone, this is my first piece of JS Code")
+
+console.log({name: "Anvesha"})
+
+console.log(true)
+console.log(false)
+
+console.log(false,234233, 'string', {name: "Anand"})
+
+console.warn("This is a warning")
+
+console.error("This is an error")
+
