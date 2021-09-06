92ebd73edf81ca21a8bce963595bc8113437306d
Anand1417 <ai6774@srmist.edu.in>
2021-09-07 02:04:59 +0530

    Conditionals start

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
-const a = 10
+const a = '99'
 
-const b = 50
+if(typeof a === 'number'){
+    if(a % 2 === 0){
+        console.log("Number is even")
+    }
+    else if(a===0){
+        console.log("Neither Even nor Odd")
+    }
+    else {
+        console.log("Number is Odd")
+    }
+}
+else {
+    console.error("Not a number")
+}
 
-const c = 35
-
-console.log(c > a && c < b)
-
-
-console.log("<------------------->")
-
-console.log("TRUTH TABLE: AND")
-
-console.log("false && false",false && false)
-console.log("true && true",true && true)
-console.log("true && false",true && false)
-console.log("false && true",false && false)
-
-console.log("TRUTH TABLE: OR")
-
-console.log("false || false",false || false)
-console.log("true || true",true || true)
-console.log("true || false",true || false)
-console.log("false || true",false || false)
