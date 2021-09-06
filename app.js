f1733e17a94066839484474820fab7473295293c
Anand1417 <ai6774@srmist.edu.in>
2021-09-07 01:53:55 +0530

    Logical Operators

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-const a = 21
-const b = 7
-const c = 21
+const a = 10
 
-console.log('a > b', a > b)
-console.log('a < b', a < b)
-console.log('a == b', a == b)
-console.log('a == c', a == c)
+const b = 50
+
+const c = 35
+
+console.log(c > a && c < b)
+
 
 console.log("<------------------->")
 
-const d = '11'
-const e = 11
+console.log("TRUTH TABLE: AND")
 
-console.log(e == d)
-console.log(e === d)
+console.log("false && false",false && false)
+console.log("true && true",true && true)
+console.log("true && false",true && false)
+console.log("false && true",false && false)
 
+console.log("TRUTH TABLE: OR")
 
-
-
-
-
+console.log("false || false",false || false)
+console.log("true || true",true || true)
+console.log("true || false",true || false)
+console.log("false || true",false || false)
