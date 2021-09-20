39bdefb74f9429dc59339afb2b327da007c5f5b9
Anand1417 <ai6774@srmist.edu.in>
2021-09-21 02:04:17 +0530

    Moving to Master Branch

--- app.js
+++ app.js
@@ -19,17 +19,34 @@ console.log(x)
 console.log(y)
 console.log(z)
 
-const obj = {
+const person = {
     fname: "Anand",
+    mname: "C",
     lname: "Iyer",
     age: 20,
-    mname: "C"
 }
 
-let {age, fname, lname, mname=""} = obj
-
+let {age, fname, lname, mname=""} = person
 console.log(age)
 console.log(fname)
-
 console.log(lname)
 console.log(mname)
+
+let add = (x,y) => console.log(x+y)
+add(10,20)
+
+
+const counter = {
+    count: 1,
+    manipulate: function() {
+        return ++(this.count)
+    },
+    key: this.count
+}
+
+console.log(counter)
+console.log(counter.manipulate())
+console.log(counter)
+console.log(counter.manipulate())
+console.log(counter)
+console.log(counter.manipulate())
